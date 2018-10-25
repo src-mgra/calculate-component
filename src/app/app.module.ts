0433c52dd7e2a93ef1a13d2d5a038c30fc1750e7
Michael Graßmann <michael.grassmann@dekra.com>
2018-10-25 16:06:33 +0200

    new design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { CircularAreaComponent } from './circular-area/circular-area.component';
 import { CalcComponentModule } from 'src/app/calc-component';
 import { CircumferenceComponent } from './circumference/circumference.component';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +19,9 @@ import { CircumferenceComponent } from './circumference/circumference.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CalcComponentModule
+    CalcComponentModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
